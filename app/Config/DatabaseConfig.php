bd49ffe5c2e3ee798bf89ecd465b7a335188b222
Lusy230 <lusyrohmadhoni03@gmail.com>
2023-11-30 10:49:43 +0700

    first commit

---
+++ app/Config/DatabaseConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig
+{
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "latwebm5";
+    public $port = 3306;
+}
